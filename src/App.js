d9546a662b6006d55f7b2c918dacfbaf178d96e5
Surya Teja <pratapreddy0808@gmail.com>
2023-03-02 15:32:42 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes , Route , Outlet } from 'react-router-dom'
+import ContextProvider from './Context/ContextProvider';
+
+import ProtectedMainRoute from './components/ProtectedMainRoute';
+import Home from './components/Home';
+import Login from './components/Login';
+import SignUp from "./components/SignUp";
+import AuthProvider from './Context/AuthProvider';
+import FoodItems from './components/FoodItems';
+import ProtectedLoginRoute from './components/ProtectedLoginRoute';
+import SharedNavbar from './components/SharedNavbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <ContextProvider>
+        <Routes>
+          <Route path='/' element={<Outlet />}>
+            <Route index element={<Home />} />
+            <Route path='signup' element={
+              <ProtectedLoginRoute>
+                <SignUp />  
+              </ProtectedLoginRoute>
+            } />
+            <Route path='login' element={
+              <ProtectedLoginRoute>
+                <Login />  
+              </ProtectedLoginRoute>
+            } />
+            <Route path='homepage' element={
+              <SharedNavbar />
+            }>
+              <Route index element={<FoodItems />} />
+            </Route>
+          </Route>
+        </Routes>
+      </ContextProvider>
+    </AuthProvider>
   );
 }
 
